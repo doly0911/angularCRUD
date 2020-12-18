af10b7cba11543dc913b3cd3d4df586eebec0095
Doly Jimenez <doly.jimenez11@gmail.com>
2020-12-18 12:15:36 -0500

    implementacion de los componentes, creacion del servicio

--- src/app/crud-add/crud-add.component.ts
+++ src/app/crud-add/crud-add.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { BusinessService } from '../business.service';
 
 @Component({
   selector: 'app-crud-add',
   templateUrl: './crud-add.component.html',
-  styleUrls: ['./crud-add.component.css']
+  styleUrls: ['./crud-add.component.css'],
 })
+
 export class CrudAddComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  angForm: FormGroup;
+  constructor(private fb: FormBuilder, private bs: BusinessService) {
+    this.createForm();
   }
-
+  createForm() {
+    this.angForm = this.fb.group({
+      person_name: ['', Validators.required],
+      business_name: ['', Validators.required],
+      business_gst_number: ['', Validators.required],
+    });
+  }
+  addBusiness(person_name, busines_name, business_gst_number) {
+    this.bs.addBusiness(person_name, busines_name, business_gst_number);
+  }
+  ngOnInit() {}
 }
